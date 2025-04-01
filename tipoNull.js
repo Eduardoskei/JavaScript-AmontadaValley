14dbd61ad050bde7de7b1c9550580639ddb809bc
Eduardo Albuquerque <eduardoalbuquerque3021@gmail.com>
2025-03-31 21:59:00 -0300

    Primeira aula de JavaScript

---
+++ tipoNull.js
@@ -0,0 +1,8 @@
+let nomeEstudante;
+console.log(typeof nomeEstudante);
+
+let telefoneEstudante = null;
+console.log(typeof telefoneEstudante)
+
+console.log(nomeEstudante + 3)
+console.log(telefoneEstudante + 3)
